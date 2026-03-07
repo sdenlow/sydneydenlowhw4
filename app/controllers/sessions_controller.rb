ead8d0bef2730765d57f25079040c7af1d03099c
sdenlow <sydney.denlow@kellogg.northwestern.edu>
2026-03-07 21:57:09 +0000

    2nd save

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,37 @@
 class SessionsController < ApplicationController
+ def index
+    @places = Place.all
+  end
+
+  def show
+    @place = Place.find_by({ "id" => params["id"] })
+    @entries = Entry.where({ "place_id" => @place["id"] })
+  end
+ 
   def new
   end
 
-  def create
+ def create
+    @user = User.find_by({ "email" => params["email"] })
+    if @user != nil
+      if BCrypt::Password.new(@user["password"]) == params["password"]
+        session["user_id"] = @user["id"]
+         flash["notice"] = "Hello, #{@user['username']}."
+        redirect_to "/places"
+      else
+        flash["notice"] = "Nope."
+        redirect_to "/login"
+      end
+    else
+      flash["notice"] = "Nope."
+      redirect_to "/login"
+    end
   end
 
   def destroy
+    session["user_id"] = nil
+    flash["notice"] = "Goodbye."
+    redirect_to "/login"
   end
 end
   
