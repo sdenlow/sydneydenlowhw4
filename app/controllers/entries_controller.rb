4a78def09826d77489b9ffff7ce9c01590792315
sdenlow <sydney.denlow@kellogg.northwestern.edu>
2026-03-13 02:03:41 +0000

    I can submit a file woohoo

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,6 +1,10 @@
 class EntriesController < ApplicationController
   before_action :require_user
 
+  def index
+    @entries = Entry.all
+  end
+
   def new
   end
 
@@ -9,6 +13,7 @@ class EntriesController < ApplicationController
     @entry["title"] = params["title"]
     @entry["description"] = params["description"]
     @entry["occurred_on"] = params["occurred_on"]
+    @entry.uploaded_image.attach(params["uploaded_image"])
     @entry["place_id"] = params["place_id"]
     @entry["user_id"] = @current_user["id"]  # Associate entry with current user
     @entry.save
